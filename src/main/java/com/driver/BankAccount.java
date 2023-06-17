e0c9052dfedb2126a06dad82dd33a9c96206e27b
aashish kumar yadav <aashish8174@gmail.com>
2023-06-17 14:37:42 +0530

    updare

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -6,26 +6,78 @@ public class BankAccount {
     private double balance;
     private double minBalance;
 
-    public BankAccount(String name, double balance, double minBalance) {
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getMinBalance() {
+        return minBalance;
+    }
+
+    public void setMinBalance(double minBalance) {
+        this.minBalance = minBalance;
+    }
+
+    public BankAccount(String name, double balance, double minBalance) {
+        this.name=name;
+        this.balance=balance;
+        this.minBalance=minBalance;
     }
 
     public String generateAccountNumber(int digits, int sum) throws Exception{
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
+        int [] accountNumber = new int [digits];
+        if(sum<=0){
+            throw new RuntimeException("Account Number can not be generated");
+        }
+        else{
+            for(int i=0;i<digits;i++){
+                if(sum>=9){
+                    accountNumber[i] = 9;
+                    sum-=9;
+                }
+                else{
+                    accountNumber[i] = sum;
+                    sum-=sum;
+                }
 
-        return null;
+            }
+        }
+        String accNumber="";
+        for(int i=0;i<digits;i++){
+            accNumber+=accountNumber[i];
+        }
+        return accNumber;
     }
 
     public void deposit(double amount) {
         //add amount to balance
+        balance+=amount;
 
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-
+       if(balance>amount){
+           balance-=amount;
+       }
+       else{
+           throw new RuntimeException("Insufficient Balance");
+       }
     }
 
 }
--- src/main/java/com/driver/CurrentAccount.java
+++ src/main/java/com/driver/CurrentAccount.java
@@ -5,15 +5,33 @@ public class CurrentAccount extends BankAccount{
 
     public CurrentAccount(String name, double balance, String tradeLicenseId) throws Exception {
         // minimum balance is 5000 by default. If balance is less than 5000, throw "Insufficient Balance" exception
-
+       super(name,balance,5000);
+       this.tradeLicenseId = tradeLicenseId;
     }
 
     public void validateLicenseId() throws Exception {
+        checkValid();
+        for(int i=1;i<tradeLicenseId.length();i++){
+            char ch1 = tradeLicenseId.charAt(i-1);
+            char ch2 = tradeLicenseId.charAt(i);
+            if(ch1==ch2)
+        }
         // A trade license Id is said to be valid if no two consecutive characters are same
         // If the license Id is valid, do nothing
         // If the characters of the license Id can be rearranged to create any valid license Id
         // If it is not possible, throw "Valid License can not be generated" Exception
 
+    }
+    public void checkValid(){
+        int frqOfChar[] = new int[26];
+        int n = (1+tradeLicenseId.length())/2;
+        for(int i=0;i<tradeLicenseId.length();i++){
+            char ch = tradeLicenseId.charAt(i);
+            int a = frqOfChar[ch-'A']++;
+            if(a>n){
+                throw new RuntimeException("Valid License can not be generated");
+            }
+        }
     }
 
 }
